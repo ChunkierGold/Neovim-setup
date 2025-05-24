3d10b0403a39090750e18c05578945781911d1c0
Luke Anderson <lukeranderson15@gmail.com>
2025-05-23 19:25:22 -0500

    updated harpoon config and treesitter config

--- after/plugin/harpoon.lua
+++ after/plugin/harpoon.lua
@@ -6,7 +6,7 @@ harpoon:setup()
 
 --vim.keymap.set("n", "<leader>a", mark.add_file)
 --vim.keymap.set("n", "C-e", ui.toggle_quick_menu2)
-vim.keymap.set("n", "<leader>a", function() harpoon:list():append() end)
+vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
 vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
 
 vim.keymap.set("n", "<C-u>", function() harpoon:list():select(1) end)
